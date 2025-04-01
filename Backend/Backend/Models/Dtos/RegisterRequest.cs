ae6e9eb164ae81b17738a1c15e7dfe67d25059b6
Christian Rodríguez Lara <christianrodriguezlara6@gmail.com>
2025-04-01 20:32:05 +0300

    Modificacion Registro, permitido añadir avatar.

--- Backend/Backend/Models/Dtos/RegisterRequest.cs
+++ Backend/Backend/Models/Dtos/RegisterRequest.cs
@@ -11,12 +11,13 @@ public class RegisterRequest
       public required string Name {get; set;}
       public string LastName {get; set;}
       public string Biography { get; set; }
-      public string AvatarUrl { get; set;  }
       public string School { get; set; }
       public string Degree { get; set; }
       public string Nationality { get; set; }
+      public IFormFile? File { get; set; }
 
-      [Phone]
+
+    [Phone]
       public required string Phone {get; set;}
       public List<SocialMediaLink> SocialMedias { get; set; } = new List<SocialMediaLink>();
 
