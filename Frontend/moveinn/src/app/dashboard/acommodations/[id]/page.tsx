a072fc83bc6cab51039ad548742a22024d41a4e9
Yasir <y.asirbelmaalem30@gmail.com>
2025-05-21 21:03:08 +0300

    deploy

--- Frontend/moveinn/src/app/dashboard/acommodations/[id]/page.tsx
+++ Frontend/moveinn/src/app/dashboard/acommodations/[id]/page.tsx
@@ -214,7 +214,6 @@ export default function AccommodationDetailsPage() {
                 <span className="text-text-secondary ml-1">/month</span>
               </div>
             </div>
-            {/*  */}
             <div className="space-y-3 mb-6">
               <div className="flex justify-between">
                 <span className="text-text-secondary">Security deposit</span>
