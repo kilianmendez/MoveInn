da38f00e5e297c3dcc6bfa06bb947c1bd81637dd
Yasir <y.asirbelmaalem30@gmail.com>
2025-05-27 20:45:28 +0300

    deploy backend

--- scripts/after_install_backend.sh
+++ scripts/after_install_backend.sh
@@ -9,11 +9,4 @@ echo "[AfterInstall] Sincronizando artefacto publicado…"
 mkdir -p "$DEST"
 rsync -a --delete "$PUBLISH_DIR/" "$DEST/"
 
-echo "[AfterInstall] Aplicando migraciones EF Core…"
-export ASPNETCORE_ENVIRONMENT=Production        
-dotnet ef database update \
-  -p "$SRC_ROOT/Backend/Backend.csproj" \
-  -s "$SRC_ROOT/Backend/Backend.csproj" \
-  --no-build
-
 chown -R www-data:www-data "$DEST"
