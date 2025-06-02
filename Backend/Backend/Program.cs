505c8ee960c2c5a1bce2aa4de82abc440705aaa5
Yasir <y.asirbelmaalem30@gmail.com>
2025-06-02 15:41:05 +0300

    deploy backend

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -158,7 +158,6 @@ public class Program
 
         app.Run();
     }
-    //Force
     static async Task SeedDatabase(IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
