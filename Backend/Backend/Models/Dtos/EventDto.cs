b741ddea91675603e57a46cd08517db21985e4c9
Yasir <y.asirbelmaalem30@gmail.com>
2025-06-11 16:48:14 +0200

    deploy backend

--- Backend/Backend/Models/Dtos/EventDto.cs
+++ Backend/Backend/Models/Dtos/EventDto.cs
@@ -19,7 +19,6 @@ namespace Backend.Models.Dtos
         public string ImageUrl { get; set; }
         public List<string> Tags { get; set; }
 
-        public Guid CreatorId { get; set; }
         public string CreatorName { get; set; }
         public string CreatorLastName { get; set; }
         public string CreatorAvatarUrl { get; set; }
